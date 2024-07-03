cde494e34c834c036f7e34b54ce97da477aedbfd
Daduli <tle311.pn@gmail.com>
2024-07-03 14:06:52 +0200

    Finished project for real this time (Forgot to add the comment in the data)

--- C/Grace.c
+++ C/Grace.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 #define FILE "Grace_kid.c"
-#define DATA "#include <stdio.h>%1$c%1$c#define FILE %2$cGrace_kid.c%2$c%1$c#define DATA %2$c%3$s%2$c%1$c#define CODE(flag) int main() {fprintf(fopen(FILE, flag), DATA, 10, 34, DATA);}%1$c%1$cCODE(%2$cw%2$c)"
+#define DATA "#include <stdio.h>%1$c%1$c#define FILE %2$cGrace_kid.c%2$c%1$c#define DATA %2$c%3$s%2$c%1$c#define CODE(flag) int main() {fprintf(fopen(FILE, flag), DATA, 10, 34, DATA);}%1$c%1$c/*%1$cMain code section%1$c*/%1$c%1$cCODE(%2$cw%2$c)"
 #define CODE(flag) int main() {fprintf(fopen(FILE, flag), DATA, 10, 34, DATA);}
 
 /*
